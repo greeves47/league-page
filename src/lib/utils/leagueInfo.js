28b6c7b9360ce14fcb71a8f0365b7bae1fb2af0b
greeves47 <123155920+greeves47@users.noreply.github.com>
2025-09-04 14:36:23 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
 export const leagueID = "1255942855143215104"; // your league ID
 export const leagueName = "No Punt Intended"; // your league name
-export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dues = 5; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
