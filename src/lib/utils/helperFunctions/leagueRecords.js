7f047e695722527f58ac3abc425fd9d9282ebd46
greeves47 <123155920+greeves47@users.noreply.github.com>
2025-09-05 11:27:44 +0100

    Update leagueRecords.js

--- src/lib/utils/helperFunctions/leagueRecords.js
+++ src/lib/utils/helperFunctions/leagueRecords.js
@@ -47,6 +47,13 @@ export const getLeagueRecords = async (refresh = false) => {
 	// league page leagueID
 	let curSeason = leagueID;
 
+	// **Exclude specific league IDs**
+	const excludedLeagueIds = ["436946484563275776", "517113763118989312"];
+	if (excludedLeagueIds.includes(curSeason)) {
+		console.log(`Excluding league ID: ${curSeason}`);
+		return {};  // Return empty object or handle exclusion
+	}
+
 	// currentYear will eventually be assigned as the most recent year
 	// that has record information (current season if past week 1,
 	// previous season if not)
